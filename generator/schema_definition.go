589bcd256e5790af94d69d8c811d0a94cb5c6659
Sajjad Hashemian <hi@hashemian.me>
2020-09-10 23:58:48 +0430

    add basic types

--- generator/schema_definition.go
+++ generator/schema_definition.go
@@ -7,6 +7,9 @@ import (
 )
 
 func SchemaDefinition(wr io.Writer, g *gen.Graph) error {
+	if err := templates.ExecuteTemplate(wr, "template/base_sdl.tmpl", nil); err != nil {
+		return err
+	}
 	for _, t := range g.Nodes {
 		if err := templates.ExecuteTemplate(wr, "template/node_sdl.tmpl", t); err != nil {
 			return err
